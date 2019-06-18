03cceebd6dccac4ce2bd7d10d88108654d188b8c
emrah karaman <ekaraman89@hotmail.com>
2019-06-18 16:44:38 +0300

    .

--- SendMail/Form1.Designer.cs
+++ SendMail/Form1.Designer.cs
@@ -28,20 +28,163 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.tmr = new System.Windows.Forms.Timer(this.components);
+            this.txtSubject = new System.Windows.Forms.TextBox();
+            this.lblSubject = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.rtxtBody = new System.Windows.Forms.RichTextBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.pBar = new System.Windows.Forms.ProgressBar();
+            this.lblTo = new System.Windows.Forms.Label();
+            this.lblToAddress = new System.Windows.Forms.Label();
+            this.lblCout = new System.Windows.Forms.Label();
+            this.lblCountDown = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.SuspendLayout();
+            // 
+            // tmr
+            // 
+            this.tmr.Interval = 60000;
+            this.tmr.Tick += new System.EventHandler(this.tmr_Tick);
+            // 
+            // txtSubject
+            // 
+            this.txtSubject.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtSubject.Location = new System.Drawing.Point(83, 56);
+            this.txtSubject.Name = "txtSubject";
+            this.txtSubject.Size = new System.Drawing.Size(786, 26);
+            this.txtSubject.TabIndex = 0;
+            // 
+            // lblSubject
+            // 
+            this.lblSubject.AutoSize = true;
+            this.lblSubject.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblSubject.Location = new System.Drawing.Point(23, 59);
+            this.lblSubject.Name = "lblSubject";
+            this.lblSubject.Size = new System.Drawing.Size(54, 20);
+            this.lblSubject.TabIndex = 1;
+            this.lblSubject.Text = "Konu :";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(23, 97);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "İçerik :";
+            // 
+            // rtxtBody
+            // 
+            this.rtxtBody.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.rtxtBody.Location = new System.Drawing.Point(84, 97);
+            this.rtxtBody.Name = "rtxtBody";
+            this.rtxtBody.Size = new System.Drawing.Size(785, 325);
+            this.rtxtBody.TabIndex = 3;
+            this.rtxtBody.Text = "";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(720, 428);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(139, 38);
+            this.btnStart.TabIndex = 4;
+            this.btnStart.Text = "Göndermeye Başla";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // pBar
+            // 
+            this.pBar.Location = new System.Drawing.Point(84, 428);
+            this.pBar.Name = "pBar";
+            this.pBar.Size = new System.Drawing.Size(630, 38);
+            this.pBar.TabIndex = 5;
+            // 
+            // lblTo
+            // 
+            this.lblTo.AutoSize = true;
+            this.lblTo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblTo.Location = new System.Drawing.Point(23, 18);
+            this.lblTo.Name = "lblTo";
+            this.lblTo.Size = new System.Drawing.Size(52, 20);
+            this.lblTo.TabIndex = 6;
+            this.lblTo.Text = "Kime :";
+            // 
+            // lblToAddress
+            // 
+            this.lblToAddress.AutoSize = true;
+            this.lblToAddress.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblToAddress.Location = new System.Drawing.Point(81, 18);
+            this.lblToAddress.Name = "lblToAddress";
+            this.lblToAddress.Size = new System.Drawing.Size(0, 20);
+            this.lblToAddress.TabIndex = 7;
+            // 
+            // lblCout
+            // 
+            this.lblCout.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblCout.Location = new System.Drawing.Point(632, 18);
+            this.lblCout.Name = "lblCout";
+            this.lblCout.Size = new System.Drawing.Size(237, 20);
+            this.lblCout.TabIndex = 8;
+            this.lblCout.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // lblCountDown
+            // 
+            this.lblCountDown.AutoSize = true;
+            this.lblCountDown.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblCountDown.Location = new System.Drawing.Point(431, 18);
+            this.lblCountDown.Name = "lblCountDown";
+            this.lblCountDown.Size = new System.Drawing.Size(47, 20);
+            this.lblCountDown.TabIndex = 9;
+            this.lblCountDown.Text = "[ XX ]";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // frmMain
             // 
+            this.AcceptButton = this.btnStart;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(886, 480);
+            this.Controls.Add(this.lblCountDown);
+            this.Controls.Add(this.lblCout);
+            this.Controls.Add(this.lblToAddress);
+            this.Controls.Add(this.lblTo);
+            this.Controls.Add(this.pBar);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.rtxtBody);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblSubject);
+            this.Controls.Add(this.txtSubject);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Name = "frmMain";
             this.Text = "Toplu Mail";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmMain_FormClosing);
+            this.Load += new System.EventHandler(this.frmMain_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Timer tmr;
+        private System.Windows.Forms.TextBox txtSubject;
+        private System.Windows.Forms.Label lblSubject;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox rtxtBody;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.ProgressBar pBar;
+        private System.Windows.Forms.Label lblTo;
+        private System.Windows.Forms.Label lblToAddress;
+        private System.Windows.Forms.Label lblCout;
+        private System.Windows.Forms.Label lblCountDown;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- SendMail/Form1.cs
+++ SendMail/Form1.cs
@@ -1,12 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Net.Mail;
+using System.Configuration;
+using System.Net.Configuration;
+using System.Net;
+using System.IO;
 
 namespace SendMail
 {
@@ -15,6 +15,142 @@ namespace SendMail
         public frmMain()
         {
             InitializeComponent();
+        }
+
+        string[] MailAddress;
+        int interval;
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            string val = ConfigurationManager.AppSettings["TimerInterval"];
+            interval = Convert.ToInt32(val);
+            tmr.Interval = interval;
+
+            string path = "MailList.txt";
+            if (!File.Exists(path))
+            {
+                File.Create(path).Close();
+            }
+            MailAddress = File.ReadAllLines(path);
+            lblCout.Text = $"{MailAddress.Length} adet mail adresi algılandı";
+            File.Delete(path);
+            pBar.Maximum = MailAddress.Length;
+        }
+        private void SendMail(string ToMailAdress)
+        {
+            Configuration oConfig = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+            var mailSettings = oConfig.GetSectionGroup("system.net/mailSettings") as MailSettingsSectionGroup;
+
+            if (mailSettings != null)
+            {
+                string result = "Başarılı";
+                try
+                {
+                    int port = mailSettings.Smtp.Network.Port;
+                    string from = mailSettings.Smtp.From;
+                    string host = mailSettings.Smtp.Network.Host;
+                    string pwd = mailSettings.Smtp.Network.Password;
+                    string uid = mailSettings.Smtp.Network.UserName;
+
+                    var message = new MailMessage
+                    {
+                        From = new MailAddress(from)
+                    };
+                    message.To.Add(new MailAddress(ToMailAdress));
+                    // message.CC.Add(new MailAddress(from));
+                    message.Subject = txtSubject.Text;
+                    message.IsBodyHtml = true;
+                    message.Body = rtxtBody.Text;
+
+                    var client = new SmtpClient
+                    {
+                        Host = host,
+                        Port = port,
+                        Credentials = new NetworkCredential(uid, pwd),
+                        EnableSsl = true
+                    };
+
+
+                    client.Send(message);
+                }
+                catch (Exception ex)
+                {
+                    result = ex.Message;
+                }
+                pBar.Value++;
+                lblCout.Text = $"{MailAddress.Length - 1} adet mail adresi kaldı";
+                Result(ToMailAdress, result);
+                tmr.Enabled = true;
+            }
+
+        }
+
+        private void Result(string ToMailAdress, string result)
+        {
+            string ResultPath = "Result.txt";
+            if (!File.Exists(ResultPath))
+            {
+                File.Create(ResultPath).Close();
+            }
+            using (StreamWriter sw = File.AppendText(ResultPath))
+            {
+                sw.WriteLine($"{ToMailAdress} {result}");
+            }
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            btnStart.Enabled = false;
+            txtSubject.Enabled = false;
+            rtxtBody.Enabled = false;
+            timer1.Start();
+        }
+
+        private void tmr_Tick(object sender, EventArgs e)
+        {
+            tmr.Enabled = false;
+            if (MailAddress.Length > 0)
+            {
+                string address = MailAddress[0];
+                lblToAddress.Text = address;
+                SendMail(address);
+                MailAddress = MailAddress.Where(x => x != MailAddress[0]).ToArray();
+            }
+            else
+            {
+                btnStart.Enabled = true;
+            }
+        }
+
+        private void frmMain_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            string path = "MailList.txt";
+            if (!File.Exists(path))
+            {
+                File.Create(path).Close();
+            }
+
+            foreach (string item in MailAddress)
+            {
+                using (StreamWriter sw = File.AppendText(path))
+                {
+                    sw.WriteLine(item);
+                }
+            }
+        }
+
+        int countdown = 1;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            int result = ((interval / 1000) - countdown++);
+            if (result > 0)
+            {
+                lblCountDown.Text = ((interval / 1000) - countdown++).ToString();
+            }
+            else
+            {
+                countdown = 1;
+                tmr_Tick(sender, e);
+            }
         }
     }
 }
